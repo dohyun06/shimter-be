1f47e18801b3422e5d71979400abf8bdc5e37dd5
Dohyun Kim <dohyun06b@gmail.com>
2025-08-18 12:55:17 +0900

    style: delete annotation

--- src/ai/ai.service.ts
+++ src/ai/ai.service.ts
@@ -11,20 +11,17 @@ export class AiService {
   async getAiResult({ url }: ImageUrlDto): Promise<any> {
     let tempFilePath: string = '';
     try {
-      // 1. URL에서 이미지 다운로드
       const response = await axios.get(url, {
         responseType: 'arraybuffer',
       });
       const imageBuffer = Buffer.from(response.data, 'binary');
 
-      // 2. 이미지를 임시 파일로 저장
       const tempDir = path.resolve(__dirname, './temp');
       if (!fs.existsSync(tempDir)) fs.mkdirSync(tempDir);
       const tempFileName = `${v4()}.jpg`;
       tempFilePath = path.join(tempDir, tempFileName);
       fs.writeFileSync(tempFilePath, imageBuffer);
 
-      // 3. Python 자식 프로세스 실행
       return await new Promise((resolve, reject) => {
         const pythonScriptPath = path.resolve(__dirname, './python/ai.py');
         const pythonProcess = spawn('python', [pythonScriptPath, tempFilePath]);
@@ -57,7 +54,6 @@ export class AiService {
         HttpStatus.INTERNAL_SERVER_ERROR,
       );
     } finally {
-      // 4. 예측 완료 후 임시 파일 삭제
       if (tempFilePath && fs.existsSync(tempFilePath)) {
         fs.unlinkSync(tempFilePath);
       }
