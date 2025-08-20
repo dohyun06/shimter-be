624163485406ac214c1801ad715973c219ab62bd
Dohyun Kim <dohyun06b@gmail.com>
2025-08-20 21:08:43 +0900

    feat: enableCors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   const config = new DocumentBuilder()
     .setTitle('shimter-be')
     .setDescription('shimter be')
